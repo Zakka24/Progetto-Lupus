d4cb353984e0423b2dc5415d29e75ce6581fd076
Zakka24 <aoukaili.zakaria@gmail.com>
2024-10-02 19:22:14 +0200

    aggiunte modifiche per usare socket.io

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 import express from 'express';
-import bcrypt from 'bcrypt'; // libreria per hashare la password
-import jwt from 'jsonwebtoken' // Libreria per creare il token
-import { tokenChecker} from './tokenChecker.js'
+import http from 'http' // import per creare il server HTTP
+import { Server } from 'socket.io' 
+import { initializeWebSocket } from './webSocket/index.js';  // Importa la logica WebSocket
+
+// import delle routes da ./routes
 import routerAuth from './routes/autenticazione.js'
 import routerUsers from './routes/utenti.js'
 import routerRoles from './routes/ruoli.js'
 import routerSessione from './routes/sessioni.js';
  
+// import di tockenChecker
+import { tokenChecker} from './tokenChecker.js'
+
 
 const app = express();
+const server = http.createServer(app)
+
 app.use(express.json())
+app.use(express.static('client'));
 //app.use(tokenChecker)
 
+// Inizializza WebSocket
+initializeWebSocket(server);
+
 // Lancia il server
-app.listen(8080, () => {
+server.listen(8080, () => {
     console.log('Server running on porto 8080')
 });
- 
 
 // Autenticazione: '/api/auth' è il prefisso per tutte le rotte di autenticazione
 app.use('/api/auth', routerAuth); 
-app.use('api/auth/logout', tokenChecker);
+//app.use('/api/auth/logout', tokenChecker);
 
 // Utenti: '/api/users' è il prefisso per tutte le rotte per gli user
 app.use('/api/users', routerUsers)
@@ -30,4 +40,3 @@ app.use('/api/roles', routerRoles)
 
 // Sessioni: '/api/sessions' è il prefisso per tutte le rotte per le sessioni
 app.use('/api/sessions', routerSessione)
-

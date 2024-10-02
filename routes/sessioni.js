d95f8e2a16df171594bbe052e65e57dbd82a864e
Zakka24 <aoukaili.zakaria@gmail.com>
2024-10-02 16:30:28 +0200

    aggiunte routes per le API create in nel file controllers/sessioni.js

---
+++ routes/sessioni.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import multer from 'multer';
+import sessioneController from '../controllers/sessioni.js';
+
+const upload = multer();
+const routerSessione = express.Router();
+
+routerSessione.post('/:admin_id', upload.none(), sessioneController.creaNuovaSessione);
+routerSessione.patch('/entra-sessione/:id_utente/:id_sessione', upload.none(), sessioneController.utenteEntraInSessione);
+routerSessione.patch('/esce-sessione/:id_utente', upload.none(), sessioneController.utenteEsceDaSessione);
+routerSessione.get('', sessioneController.getSessioni);
+routerSessione.get('/:sessione_id', sessioneController.getSessione);
+
+export default routerSessione
